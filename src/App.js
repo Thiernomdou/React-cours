d367c40e0737832a858c5748dd99f75838a815d6
DIALLO Thierno Mamadou <thiernomdou63@gmail.com>
2022-02-01 17:10:24 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
-import Item from "./Components/Item/Item";
-
+// import Item from "./Components/Item/Item";
+import { useState } from "react";
 function App() {
 
   /*le state c'est tous les données d'un composant
   et si le state change, le composant se met à jour*/
 
+  // const [toggle, setToggle] = useState(true);
 
-  // console.log(useState(10));
+  // const changeState = () => {
+  //   setToggle(!toggle);
+  // }
+
+  // const [dataArr, setDataArr] = useState([
+  //   {nom: "Juliette"},
+  //   {nom: "John"},
+  //   {nom: "Joris"},
+  // ]);
 
   return (
     <div className="App">
-      <h1>Hello depuis App</h1>
-      <Item />
+      {/* {toggle ? <h1>Le state est True</h1> :
+        <h1>Le state est False</h1>} */
+        // <div className={toggle ? "box animated" : "box"}></div>
+        //map crée un nouveau tableau avec tous les
+        //éléments à retourner à l'intérieur
+        // dataArr.map((item,index) => {
+        //   return <p key={index}>Nom: {item.nom}</p>
+        // })
+      }
     </div>
   );
 }
